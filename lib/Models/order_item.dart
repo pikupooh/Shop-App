32da2b1357867d4aa54dc6dd399079724cc85f61
kishanhitk <kishansharma1231@gmail.com>
2020-05-07 14:20:41 +0530

    Added more information in OrderItem model. Made UI for PastOrders Page.

--- lib/Models/order_item.dart
+++ lib/Models/order_item.dart
@@ -1,19 +1,36 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-class OrderItem{
+class OrderItem {
+  String status;
+  String orderID;
   String userid;
   String totalCartCost;
   Map<String, int> items;
+  Timestamp orderDate;
 
-  OrderItem({this.userid, this.totalCartCost});
+  OrderItem(
+      {this.userid,
+      this.totalCartCost,
+      this.orderID,
+      this.orderDate,
+      this.status});
 
-  factory OrderItem.fromFirebase(DocumentSnapshot doc){
-    Map data = doc.data ;
+  factory OrderItem.fromFirebase(DocumentSnapshot doc) {
+    String orderID = doc.documentID;
+    Map data = doc.data;
     // print(data.toString());
-    OrderItem orderItem = new OrderItem (userid: data['userid'], totalCartCost: data['totalCartCost']);
+    OrderItem orderItem = new OrderItem(
+        status: data['status'],
+        orderDate: data['orderDate'],
+        orderID: orderID,
+        userid: data['userid'],
+        totalCartCost: data['totalCartCost']);
     orderItem.items = new Map<String, int>();
-    data.forEach((key, value){
-      if(key != 'userid' && key != 'totalCartCost'){
+    data.forEach((key, value) {
+      if (key != 'userid' &&
+          key != 'totalCartCost' &&
+          key != 'orderDate' &&
+          key != 'status') {
         // print(value);
         orderItem.items.addAll({"$key": value});
       }
