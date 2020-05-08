4b3fb5a4d2c1497a94edb0f55b4b19b6807c8aa5
kishanhitk <kishansharma1231@gmail.com>
2020-05-08 11:26:55 +0530

    Added network connectivity check on registration page

--- lib/Pages/register_page.dart
+++ lib/Pages/register_page.dart
@@ -4,6 +4,7 @@ import 'package:fluttertoast/fluttertoast.dart';
 import 'package:shop_app/Services/auth.dart';
 import 'package:shop_app/reusables/components.dart';
 import 'package:shop_app/reusables/constants.dart';
+import 'dart:io';
 
 class RegisterPage extends StatefulWidget {
   @override
@@ -18,9 +19,28 @@ class _RegisterPageState extends State<RegisterPage> {
   bool verComplete = false;
   bool readonly = false;
   bool sendingOtp = false;
+  bool isConnected = true;
+
+  void checkNetwok() async {
+    try {
+      final result = await InternetAddress.lookup('google.com');
+      if (result.isNotEmpty && result[0].rawAddress.isNotEmpty) {
+        setState(() {
+          isConnected = true;
+        });
+        print('connected');
+      }
+    } on SocketException catch (_) {
+      setState(() {
+        isConnected = false;
+      });
+      print('not connected');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
+    checkNetwok();
     return Scaffold(
       backgroundColor: kbackgroundColor,
       body: verComplete
@@ -40,123 +60,129 @@ class _RegisterPageState extends State<RegisterPage> {
                   SizedBox(
                     height: 40,
                   ),
-                  Form(
-                    key: formkey,
-                    child: Padding(
-                      padding: const EdgeInsets.all(30.0),
-                      child: Column(
-                        children: <Widget>[
-                     
-                          smsSent
-                              ? Container()
-                              : SizedBox(
-                                  height: 10,
-                                ),
-                          smsSent
-                              ? Container()
-                              : TextFormField(
-                                  decoration: kInputDecoration.copyWith(
-                                      fillColor: Colors.grey[300],
-                                      prefixIcon: Icon(Icons.phone),
-                                      // prefixText: "+91-",
-                                      hintText: "Phone"),
-                                  keyboardType: TextInputType.phone,
-                                  onChanged: (value) {
-                                    setState(() {
-                                      phone = "+91" + value;
-                                    });
-                                  },
-                                  validator: (String value) {
-                                    if (value.isEmpty || value.length != 10)
-                                      return "Enter 10 digits";
-                                    return null;
-                                  },
-                                ),
-                          SizedBox(height: 10),
-                          Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: sendingOtp
-                                ? Container(
-                                    child: Column(
-                                      children: <Widget>[
-                                        CircularProgressIndicator(
-                                          backgroundColor: Color(0xFF0013A8),
-                                        ),
-                                        SizedBox(
-                                          height: 4,
-                                        ),
-                                        Text("Sending OTP.")
-                                      ],
-                                    ),
-                                  )
-                                : smsSent
+                  isConnected
+                      ? Form(
+                          key: formkey,
+                          child: Padding(
+                            padding: const EdgeInsets.all(30.0),
+                            child: Column(
+                              children: <Widget>[
+                                smsSent
                                     ? Container()
-                                    : Container(
+                                    : SizedBox(
+                                        height: 10,
+                                      ),
+                                smsSent
+                                    ? Container()
+                                    : TextFormField(
+                                        decoration: kInputDecoration.copyWith(
+                                            fillColor: Colors.grey[300],
+                                            prefixIcon: Icon(Icons.phone),
+                                            // prefixText: "+91-",
+                                            hintText: "Phone"),
+                                        keyboardType: TextInputType.phone,
+                                        onChanged: (value) {
+                                          setState(() {
+                                            phone = "+91" + value;
+                                          });
+                                        },
+                                        validator: (String value) {
+                                          if (value.isEmpty ||
+                                              value.length != 10)
+                                            return "Enter 10 digits";
+                                          return null;
+                                        },
+                                      ),
+                                SizedBox(height: 10),
+                                Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: sendingOtp
+                                      ? Container(
+                                          child: Column(
+                                            children: <Widget>[
+                                              CircularProgressIndicator(
+                                                backgroundColor:
+                                                    Color(0xFF0013A8),
+                                              ),
+                                              SizedBox(
+                                                height: 4,
+                                              ),
+                                              Text("Sending OTP.")
+                                            ],
+                                          ),
+                                        )
+                                      : smsSent
+                                          ? Container()
+                                          : Container(
+                                              decoration: kSoftShadowDecoration
+                                                  .copyWith(
+                                                      borderRadius:
+                                                          BorderRadius.circular(
+                                                              50)),
+                                              child: Buttons(
+                                                icon: Icons.arrow_forward_ios,
+                                                buttonColor: kbackgroundColor,
+                                                text: "Send OTP",
+                                                iconColor: Color(0xFF0013A8),
+                                                textColor: Color(0xFF0013A8),
+                                                onTap: () async {
+                                                  if (formkey.currentState
+                                                      .validate()) {
+                                                    print(phone);
+                                                    setState(() {
+                                                      readonly = true;
+                                                    });
+                                                    await verifyPhone(phone);
+                                                    setState(() {
+                                                      sendingOtp = true;
+                                                    });
+                                                  }
+                                                },
+                                              ),
+                                            ),
+                                ),
+                                smsSent
+                                    ? TextFormField(
+                                        textAlign: TextAlign.center,
+                                        decoration: kInputDecoration.copyWith(
+                                          fillColor: Colors.grey[300],
+                                        ),
+                                        keyboardType: TextInputType.number,
+                                        onChanged: (value) {
+                                          setState(() {
+                                            smsCode = value;
+                                          });
+                                        },
+                                      )
+                                    : Container(),
+                                SizedBox(
+                                  height: 20,
+                                ),
+                                smsSent
+                                    ? Container(
                                         decoration:
                                             kSoftShadowDecoration.copyWith(
                                                 borderRadius:
                                                     BorderRadius.circular(50)),
                                         child: Buttons(
-                                          icon: Icons.arrow_forward_ios,
+                                          iconColor: Colors.black,
+                                          textColor: Colors.black,
+                                          icon: Icons.verified_user,
                                           buttonColor: kbackgroundColor,
-                                          text: "Send OTP",
-                                          iconColor: Color(0xFF0013A8),
-                                          textColor: Color(0xFF0013A8),
-                                          onTap: () async {
-                                            if (formkey.currentState
-                                                .validate()) {
-                                              print(phone);
-                                              setState(() {
-                                                readonly = true;
-                                              });
-                                              await verifyPhone(phone);
-                                              setState(() {
-                                                sendingOtp = true;
-                                              });
-                                            }
+                                          text: "Verify OTP",
+                                          onTap: () {
+                                            print(phone);
+                                            AuthServices().signInOTP(smsCode,
+                                                verificationID, name, phone);
                                           },
                                         ),
-                                      ),
+                                      )
+                                    : Container(),
+                              ],
+                            ),
                           ),
-                          smsSent
-                              ? TextFormField(
-                                  textAlign: TextAlign.center,
-                                  decoration: kInputDecoration.copyWith(
-                                    fillColor: Colors.grey[300],
-                                  ),
-                                  keyboardType: TextInputType.number,
-                                  onChanged: (value) {
-                                    setState(() {
-                                      smsCode = value;
-                                    });
-                                  },
-                                )
-                              : Container(),
-                          SizedBox(
-                            height: 20,
-                          ),
-                          smsSent
-                              ? Container(
-                                  decoration: kSoftShadowDecoration.copyWith(
-                                      borderRadius: BorderRadius.circular(50)),
-                                  child: Buttons(
-                                    iconColor: Colors.black,
-                                    textColor: Colors.black,
-                                    icon: Icons.verified_user,
-                                    buttonColor: kbackgroundColor,
-                                    text: "Verify OTP",
-                                    onTap: () {
-                                      print(phone);
-                                      AuthServices().signInOTP(
-                                          smsCode, verificationID, name, phone);
-                                    },
-                                  ),
-                                )
-                              : Container(),
-                        ],
-                      ),
-                    ),
-                  ),
+                        )
+                      : Center(child: Text("Please connect to internet to use the app.")),
                 ],
               ),
             ),
