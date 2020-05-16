e2cf3fb2f6ea8331103a145521307048706e0daa
kishanhitk <kishansharma1231@gmail.com>
2020-05-16 18:15:27 +0530

    Added Product Description Page.

--- lib/Pages/productDetailspage.dart
+++ lib/Pages/productDetailspage.dart
@@ -1,16 +1,204 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
 import 'package:shop_app/Models/product.dart';
+import 'package:shop_app/Models/user.dart';
+import 'package:shop_app/Services/database.dart';
+import 'package:shop_app/reusables/components.dart';
+import 'package:shop_app/reusables/constants.dart';
+
+import 'cart_page.dart';
 
 class ProductDetails extends StatefulWidget {
   final Product product;
+
   ProductDetails({this.product});
   @override
   _ProductDetailsState createState() => _ProductDetailsState();
 }
 
 class _ProductDetailsState extends State<ProductDetails> {
+  void _showToast(String name) {
+    Fluttertoast.showToast(
+        msg: "$name added to cart",
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.BOTTOM);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    var user = Provider.of<User>(context);
+    return Scaffold(
+      floatingActionButton: Container(
+        decoration: kSoftShadowDecoration,
+        child: Buttons(
+          onTap: () async {
+            DatabaseServices().addToCart(widget.product, user);
+            _showToast(widget.product.name);
+          },
+          iconColor: Colors.red,
+          textColor: Colors.red,
+          buttonColor: kbackgroundColor,
+          text: "Add to Cart",
+          icon: CupertinoIcons.add_circled_solid,
+        ),
+      ),
+      backgroundColor: kbackgroundColor,
+      appBar: AppBar(
+        actions: <Widget>[
+          StreamBuilder(
+              stream: DatabaseServices().getCartItems(user),
+              builder: (context, snapshot) {
+                return Container(
+                  child: Stack(
+                    children: <Widget>[
+                      Center(
+                        child: IconButton(
+                          icon: Icon(
+                            CupertinoIcons.shopping_cart,
+                            color: Colors.black,
+                          ),
+                          onPressed: () {
+                            Navigator.push(
+                                context,
+                                CupertinoPageRoute(
+                                    builder: (context) => CartPage()));
+                          },
+                        ),
+                      ),
+                      Positioned(
+                        top: 9,
+                        right: 4,
+                        child: Container(
+                            child: Center(
+                              child: Text(
+                                snapshot.data != null
+                                    ? snapshot.data.length.toString()
+                                    : '0',
+                                style: TextStyle(
+                                    color: Colors.white, fontSize: 10),
+                              ),
+                            ),
+                            height: 15,
+                            width: 15,
+                            decoration: BoxDecoration(
+                              color: Colors.red,
+                              shape: BoxShape.circle,
+                            )),
+                      )
+                    ],
+                  ),
+                );
+              })
+        ],
+        centerTitle: true,
+        leading: IconButton(
+            icon: Icon(
+              CupertinoIcons.back,
+              color: Colors.black,
+            ),
+            onPressed: () {
+              Navigator.pop(context);
+            }),
+        backgroundColor: kbackgroundColor,
+        elevation: 0,
+        title: Text(
+         ""?? widget.product.name,
+          style: TextStyle(fontSize: 20),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(18.0),
+          child: Container(
+            decoration: kSoftShadowDecoration.copyWith(
+                borderRadius: BorderRadius.circular(5)),
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Container(
+                      // width: MediaQuery.of(context).size.width,
+                      // height: MediaQuery.of(context).size.width,
+                      child: ClipRRect(
+                          borderRadius: BorderRadius.circular(5),
+                          child: Hero(
+                            tag: widget.product.imageurl,
+                            child: CachedNetworkImage(
+                              imageUrl: widget.product.imageurl,
+                              fit: BoxFit.contain,
+                            ),
+                          )),
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: Align(
+                          alignment: Alignment.centerLeft,
+                          child: Text(
+                            widget.product.name,
+                            style: TextStyle(
+                                fontSize: 40, fontWeight: FontWeight.w600),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: RichText(
+                          text: TextSpan(
+                              text: '₹ ',
+                              style: TextStyle(color: Colors.redAccent),
+                              children: <TextSpan>[
+                                TextSpan(
+                                    text: widget.product.cost,
+                                    style: GoogleFonts.lato(
+                                        fontWeight: FontWeight.w400,
+                                        color: Colors.black,
+                                        fontSize: 29)),
+                              ]),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 10.0),
+                    child: Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        widget.product.description,
+                        style: TextStyle(fontSize: 20, color: Colors.grey),
+                      ),
+                    ),
+                  ),
+                  // Padding(
+                  //   padding: const EdgeInsets.only(left: 10.0, top: 10),
+                  //   child: Align(
+                  //     alignment: Alignment.centerLeft,
+                  //     child: Text(
+                  //       "Description",
+                  //       style: TextStyle(fontSize: 15),
+                  //     ),
+                  //   ),
+                  // ),
+                  Divider(),
+                  Text(
+                    "lorem Ipsum",
+                    style: TextStyle(fontSize: 16),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
--- lib/Widgets/product_list.dart
+++ lib/Widgets/product_list.dart
@@ -67,88 +67,89 @@ class _ProductListState extends State<ProductList> {
     User user = Provider.of<User>(context);
     return Padding(
       padding: const EdgeInsets.all(9.0),
-      child: Container(
-        decoration: BoxDecoration(
-            color: kbackgroundColor,
-            boxShadow: [
-              BoxShadow(
-                  color: kshadowColor, offset: Offset(8, 6), blurRadius: 12),
-              BoxShadow(
-                  color: klightShadowColor,
-                  offset: Offset(-8, -6),
-                  blurRadius: 12),
-            ],
-            borderRadius: BorderRadius.circular(15)),
-        child: FittedBox(
-          fit: BoxFit.contain,
-          child: Column(
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Container(
-                  height: 120,
-                  width: 150,
-                  child: ClipRRect(
-                      borderRadius: BorderRadius.circular(15),
-                      child: CachedNetworkImage(
-                        imageUrl: product.imageurl,
-                        fit: BoxFit.cover,
-                      )),
+      child: GestureDetector(
+        onTap: () {
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) => ProductDetails(
+                        product: product,
+                      )));
+        },
+        child: Container(
+          decoration: BoxDecoration(
+              color: kbackgroundColor,
+              boxShadow: [
+                BoxShadow(
+                    color: kshadowColor, offset: Offset(8, 6), blurRadius: 12),
+                BoxShadow(
+                    color: klightShadowColor,
+                    offset: Offset(-8, -6),
+                    blurRadius: 12),
+              ],
+              borderRadius: BorderRadius.circular(15)),
+          child: FittedBox(
+            fit: BoxFit.contain,
+            child: Column(
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    height: 120,
+                    width: 150,
+                    child: ClipRRect(
+                        borderRadius: BorderRadius.circular(15),
+                        child: Hero(
+                          tag: product.imageurl,
+                          child: CachedNetworkImage(
+                            imageUrl: product.imageurl,
+                            fit: BoxFit.cover,
+                          ),
+                        )),
+                  ),
                 ),
-              ),
-              Align(
-                  alignment: Alignment.centerLeft,
-                  child: Text(
-                    product.name,
-                    style: GoogleFonts.manrope(
-                        fontSize: 23, fontWeight: FontWeight.w400),
-                  )),
-              Align(
-                  alignment: Alignment.centerLeft,
-                  child: Padding(
-                    padding: const EdgeInsets.only(top: 10),
+                Align(
+                    alignment: Alignment.centerLeft,
                     child: Text(
-                      product.description,
-                      style: TextStyle(color: Colors.grey),
-                    ),
-                  )),
-              SizedBox(height: 10),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  Align(
+                      product.name,
+                      style: GoogleFonts.manrope(
+                          fontSize: 23, fontWeight: FontWeight.w400),
+                    )),
+                Align(
                     alignment: Alignment.centerLeft,
                     child: Padding(
-                      padding: const EdgeInsets.only(left: 20.0, top: 5),
-                      child: RichText(
-                        text: TextSpan(
-                            text: '₹ ',
-                            style: TextStyle(color: Colors.redAccent),
-                            children: <TextSpan>[
-                              TextSpan(
-                                  text: product.cost,
-                                  style: GoogleFonts.lato(
-                                      fontWeight: FontWeight.w400,
-                                      color: Colors.black,
-                                      fontSize: 29)),
-                            ]),
+                      padding: const EdgeInsets.only(top: 10),
+                      child: Text(
+                        product.description,
+                        style: TextStyle(color: Colors.grey),
+                      ),
+                    )),
+                SizedBox(height: 10),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Align(
+                      alignment: Alignment.centerLeft,
+                      child: Padding(
+                        padding: const EdgeInsets.only(left: 20.0, top: 5),
+                        child: RichText(
+                          text: TextSpan(
+                              text: '₹ ',
+                              style: TextStyle(color: Colors.redAccent),
+                              children: <TextSpan>[
+                                TextSpan(
+                                    text: product.cost,
+                                    style: GoogleFonts.lato(
+                                        fontWeight: FontWeight.w400,
+                                        color: Colors.black,
+                                        fontSize: 29)),
+                              ]),
+                        ),
                       ),
                     ),
-                  ),
-                  SizedBox(width: 10),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: InkWell(
-                      onTap: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => ProductDetails(
-                              product: product,
-                            ),
-                          ),
-                        );
-                      },
+                    SizedBox(width: 10),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
                       child: Container(
                         decoration: BoxDecoration(
                           shape: BoxShape.circle,
@@ -177,29 +178,29 @@ class _ProductListState extends State<ProductList> {
                             }),
                       ),
                     ),
-                  ),
-                  // Padding(
-                  //   padding: const EdgeInsets.symmetric(
-                  //       horizontal: 10.0, vertical: 5),
-                  //   child: Material(
-                  //     elevation: 7,
-                  //     shape: CircleBorder(),
-                  //     child: CircleAvatar(
-                  //       backgroundColor: Colors.orangeAccent,
-                  //       child: IconButton(
-                  //           icon: Icon(
-                  //             CupertinoIcons.add,
-                  //             color: Colors.black,
-                  //           ),
-                  //           onPressed: () {
-                  //             DatabaseServices().addToCart(product, user);
-                  //           }),
-                  //     ),
-                  //   ),
-                  // )
-                ],
-              )
-            ],
+                    // Padding(
+                    //   padding: const EdgeInsets.symmetric(
+                    //       horizontal: 10.0, vertical: 5),
+                    //   child: Material(
+                    //     elevation: 7,
+                    //     shape: CircleBorder(),
+                    //     child: CircleAvatar(
+                    //       backgroundColor: Colors.orangeAccent,
+                    //       child: IconButton(
+                    //           icon: Icon(
+                    //             CupertinoIcons.add,
+                    //             color: Colors.black,
+                    //           ),
+                    //           onPressed: () {
+                    //             DatabaseServices().addToCart(product, user);
+                    //           }),
+                    //     ),
+                    //   ),
+                    // )
+                  ],
+                )
+              ],
+            ),
           ),
         ),
       ),
