ac177ee73b8e10e6f8974854778253e7058bf77d
kishanhitk <kishansharma1231@gmail.com>
2020-05-16 13:30:04 +0530

    Added Order Status and StatusUpdate date in Order Receipt.

--- lib/Pages/orderDetails.dart
+++ lib/Pages/orderDetails.dart
@@ -39,181 +39,180 @@ class _OrderDetailsState extends State<OrderDetails> {
         child: Padding(
           padding: const EdgeInsets.all(20.0),
           child: ConstrainedBox(
-              constraints:
-                  BoxConstraints(maxWidth: MediaQuery.of(context).size.width/1.00),
-              child: Container(
-                  decoration: kSoftShadowDecoration.copyWith(
-          borderRadius: BorderRadius.circular(15)),
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.only(top: 20.0, bottom: 10),
-            child: RichText(
-              text: TextSpan(
-                text: 'OrderID:  ',
-                style: GoogleFonts.questrial(
-                    color: Colors.black54, fontSize: 15),
-                children: <TextSpan>[
-                  TextSpan(
-                      text: widget.orderItem.orderID,
-                      style: GoogleFonts.lato(
-                          fontWeight: FontWeight.w400,
-                          color: Colors.black,
-                          fontSize: 16)),
-                ],
-              ),
-            ),
-          ),
-          Divider(
-            color: Colors.black54,
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 10.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: <Widget>[
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: <Widget>[
-                    Text(
-                      "Placed on :",
-                      style: TextStyle(color: Colors.black54),
-                    ),
-                    Text(
-                      "${widget.orderItem.orderDate.toDate().day}-${widget.orderItem.orderDate.toDate().month}-${widget.orderItem.orderDate.toDate().year}",
-                      style: TextStyle(fontSize: 17),
-                    )
-                  ],
-                ),
-                Column(
+            constraints: BoxConstraints(
+                maxWidth: MediaQuery.of(context).size.width / 1.00),
+            child: Container(
+                decoration: kSoftShadowDecoration.copyWith(
+                    borderRadius: BorderRadius.circular(15)),
+                child: Column(
                   mainAxisSize: MainAxisSize.min,
-                  crossAxisAlignment: CrossAxisAlignment.center,
                   children: <Widget>[
-                    Text(
-                      "Delivered on :",
-                      style: TextStyle(color: Colors.black54),
-                    ),
-                    Text(
-                      "Yet to be delivered",
-                      style: TextStyle(fontSize: 17),
-                    )
-                  ],
-                )
-              ],
-            ),
-          ),
-          Divider(
-            color: Colors.black54,
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Text(
-              "Items",
-              style: TextStyle(fontSize: 22),
-            ),
-          ),
-          Flexible(
-            child: ListView.builder(
-              physics: ScrollPhysics(),
-              shrinkWrap: true,
-              itemCount: widget.orderItem.items.length,
-              itemBuilder: (BuildContext context, int index) {
-                String key =
-                    widget.orderItem.items.keys.elementAt(index);
-                return Padding(
-                  padding:
-                      const EdgeInsets.only(left: 20.0, top: 10),
-                  child: Column(
-                    children: <Widget>[
-                      Row(
-                        crossAxisAlignment:
-                            CrossAxisAlignment.center,
-                        mainAxisAlignment:
-                            MainAxisAlignment.spaceBetween,
-                        children: <Widget>[
-                          Text(
-                            "$key ",
-                            style: GoogleFonts.questrial(
-                                color: Colors.black, fontSize: 20),
-                          ),
-                          Padding(
-                            padding:
-                                const EdgeInsets.only(right: 20.0),
-                            child: Text(
-                                " ${widget.orderItem.items[key]}",
+                    Padding(
+                      padding: const EdgeInsets.only(top: 20.0, bottom: 10),
+                      child: RichText(
+                        text: TextSpan(
+                          text: 'OrderID:  ',
+                          style: GoogleFonts.questrial(
+                              color: Colors.black54, fontSize: 15),
+                          children: <TextSpan>[
+                            TextSpan(
+                                text: widget.orderItem.orderID,
                                 style: GoogleFonts.lato(
                                     fontWeight: FontWeight.w400,
-                                    color: Colors.black54,
+                                    color: Colors.black,
                                     fontSize: 16)),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Divider(
+                      color: Colors.black54,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 10.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: <Widget>[
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: <Widget>[
+                              Text(
+                                "Placed on :",
+                                style: TextStyle(color: Colors.black54),
+                              ),
+                              Text(
+                                "${widget.orderItem.orderDate.toDate().day}-${widget.orderItem.orderDate.toDate().month}-${widget.orderItem.orderDate.toDate().year}",
+                                style: TextStyle(fontSize: 17),
+                              )
+                            ],
                           ),
+                          Column(
+                            mainAxisSize: MainAxisSize.min,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: <Widget>[
+                              Text(
+                                "Status:",
+                                style: TextStyle(color: Colors.black54),
+                              ),
+                              Text(
+                                widget.orderItem.status == "Not Delivered"
+                                    ? "Yet to be delivered"
+                                    : "${widget.orderItem.status} on\n${widget.orderItem.statusUpdatedOn.toDate().day}-${widget.orderItem.statusUpdatedOn.toDate().month}-${widget.orderItem.statusUpdatedOn.toDate().year}",
+                                style: TextStyle(fontSize: 17),
+                              )
+                            ],
+                          )
                         ],
                       ),
-                      Divider()
-                    ],
-                  ),
-                );
-              },
-            ),
+                    ),
+                    Divider(
+                      color: Colors.black54,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        "Items",
+                        style: TextStyle(fontSize: 22),
+                      ),
+                    ),
+                    Flexible(
+                      child: ListView.builder(
+                        physics: ScrollPhysics(),
+                        shrinkWrap: true,
+                        itemCount: widget.orderItem.items.length,
+                        itemBuilder: (BuildContext context, int index) {
+                          String key =
+                              widget.orderItem.items.keys.elementAt(index);
+                          return Padding(
+                            padding: const EdgeInsets.only(left: 20.0, top: 10),
+                            child: Column(
+                              children: <Widget>[
+                                Row(
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: <Widget>[
+                                    Text(
+                                      "$key ",
+                                      style: GoogleFonts.questrial(
+                                          color: Colors.black, fontSize: 20),
+                                    ),
+                                    Padding(
+                                      padding:
+                                          const EdgeInsets.only(right: 20.0),
+                                      child: Text(
+                                          " ${widget.orderItem.items[key]}",
+                                          style: GoogleFonts.lato(
+                                              fontWeight: FontWeight.w400,
+                                              color: Colors.black54,
+                                              fontSize: 16)),
+                                    ),
+                                  ],
+                                ),
+                                Divider()
+                              ],
+                            ),
+                          );
+                        },
+                      ),
+                    ),
+                    Divider(
+                      color: Colors.black54,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        "Delivery Address",
+                        style: TextStyle(color: Colors.black54),
+                      ),
+                    ),
+                    Text(
+                      widget.orderItem.name ?? "",
+                      style: TextStyle(fontSize: 17),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(2.0),
+                      child: Text(widget.orderItem.address ?? ""),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(2.0),
+                      child: Text(widget.orderItem.phone ?? ""),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(2.0),
+                      child: Text(widget.orderItem.alternatePhone ?? ""),
+                    ),
+                    Divider(
+                      color: Colors.black54,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 20.0, top: 10),
+                      child: Container(
+                        decoration: kSoftShadowDecoration,
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(
+                              vertical: 10.0, horizontal: 20),
+                          child: RichText(
+                            text: TextSpan(
+                              text: 'Order Total: ₹  ',
+                              style: GoogleFonts.questrial(
+                                  color: Colors.green, fontSize: 20),
+                              children: <TextSpan>[
+                                TextSpan(
+                                    text: widget.orderItem.totalCartCost,
+                                    style: GoogleFonts.lato(
+                                        fontWeight: FontWeight.w400,
+                                        color: Colors.black,
+                                        fontSize: 22)),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                )),
           ),
-          Divider(
-            color: Colors.black54,
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Text(
-              "Delivery Address",
-              style: TextStyle(color: Colors.black54),
-            ),
-          ),
-          Text(
-            widget.orderItem.name ?? "",
-            style: TextStyle(fontSize: 17),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(2.0),
-            child: Text(widget.orderItem.address ?? ""),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(2.0),
-            child: Text(widget.orderItem.phone ?? ""),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(2.0),
-            child: Text(widget.orderItem.alternatePhone ?? ""),
-          ),
-          Divider(
-            color: Colors.black54,
-          ),
-   
-          Padding(
-            padding: const EdgeInsets.only(bottom: 20.0, top: 10),
-            child: Container(
-              decoration: kSoftShadowDecoration,
-              child: Padding(
-                padding: const EdgeInsets.symmetric(
-                    vertical: 10.0, horizontal: 20),
-                child: RichText(
-                  text: TextSpan(
-                    text: 'Order Total: ₹  ',
-                    style: GoogleFonts.questrial(
-                        color: Colors.green, fontSize: 20),
-                    children: <TextSpan>[
-                      TextSpan(
-                          text: widget.orderItem.totalCartCost,
-                          style: GoogleFonts.lato(
-                              fontWeight: FontWeight.w400,
-                              color: Colors.black,
-                              fontSize: 22)),
-                    ],
-                  ),
-                ),
-              ),
-            ),
-          ),
-                    ],
-                  )),
-            ),
         ),
       ),
     );
